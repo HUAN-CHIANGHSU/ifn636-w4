c75ccb1dac9ae15d794370bbc9626101f955728c
HUAN-CHIANGHSU <n11445203@qut.edu.au>
2026-04-18 15:19:50 +0000

    new

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://13.211.0.100:5001', // local
-  baseURL: 'http://3.25.108.22:5001', 
+  baseURL: 'localhost:5001', 
   headers: { 'Content-Type': 'application/json' },
 });
 
